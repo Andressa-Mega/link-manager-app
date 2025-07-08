866b555e95b24a72e8b304d01d6fe5e17d9eee85
Andressa Mega <andressa.mega98@gmail.com>
2025-07-08 19:23:07 +0200

    add styles and variables

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -1,19 +1,20 @@
-import { Text, Pressable } from 'react-native';
+import { Text, Pressable, PressableProps } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import { colors } from '@/styles/colors';
 import { styles } from './styles';
 
-type Props = {
+type Props = PressableProps & {
     name: string;
+    isSelected?: boolean;
     icon: keyof typeof MaterialIcons.glyphMap;
 }
 
-export function Category(props: Props) {
+export function Category({name, icon, isSelected, ...rest}: Props) {
   return (
-    <Pressable style={styles.container}>
-      <MaterialIcons name={props.icon} size={24} color={colors.gray[400]} />
-      <Text style={styles.name}>{props.name}</Text>
+    <Pressable style={styles.container} {...rest}>
+      <MaterialIcons name={icon} size={24} color={colors.gray[400]} />
+      <Text style={styles.name}>{name}</Text>
     </Pressable>
   );
 }
